b48247377f67df2cb325ac5034750abe4a819892
Shubham Mandal <shubm23@gmail.com>
2022-07-12 01:33:48 +0530

    Created Custom Hook with localStorage

--- src/Counter.js
+++ src/Counter.js
@@ -1,24 +1,41 @@
 import React, { useState, useEffect } from 'react';
+
+
+
+/* Creating Custom Hook with LocalStorage */
+const useLocalStorage = (intialState, key) => {
+  const get = () =>
+    JSON.parse(localStorage.getItem(key))
+      ? JSON.parse(localStorage.getItem(key)).value
+      : intialState;
+  const [value, setValue] = useState(get());
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify({ value }));
+  }, [value]);
+
+  return [value, setValue];
+};
+
+
+
 const Counter = ({ max, step }) => {
-  const [count, setCount] = useState(0);
-  const [resetVal, setResetVal] = useState(false);
-  const [timmer, setTimmer] = useState(null);
+  const [count, setCount] = useLocalStorage(0, 'countState');
+  // const [resetVal, setResetVal] = useState(false);
 
+  /* Will be Called whenever the count state changes */
   useEffect(() => {
     document.title = `Counter: ${count}`;
-  }, []);
-
-  useEffect(() => {
-    document.title = `Counter: ${count}`;
-    if (resetVal) {
-      const t = setTimeout(() => {
-        setResetVal(false);
-      }, 3000);
-      return () => {
-        clearTimeout(t);
-      };
-    }
   }, [count]);
+
+  /* Will be Called whenever the reset happens */
+  // useEffect(() => {
+  //   const timmer = setTimeout(() => {
+  //     setResetVal(false);
+  //   }, 10000);
+  //   return () => {
+  //     clearTimeout(timmer);
+  //   };
+  // }, [resetVal]);
 
   const increment = () => {
     setCount((c) => (c >= max ? c : c + step));
@@ -30,24 +47,26 @@ const Counter = ({ max, step }) => {
   };
 
   const reset = () => {
-    setResetVal(true);
+    // setResetVal(true);
     setCount(0);
   };
 
-  if (resetVal) {
-    return <div>Resetting the value ....</div>;
-  } else {
-    return (
-      <div className="Counter">
-        <p className="count">{count}</p>
-        <section className="controls">
-          <button onClick={increment}>Increment</button>
-          <button onClick={decrement}>Decrement</button>
-          <button onClick={reset}>Reset</button>
-        </section>
-      </div>
-    );
-  }
+  // if (resetVal)
+  //   return (
+  //     <div style={{ display: 'flex', alignItems: 'center' }}>
+  //       Resetting the value ....
+  //     </div>
+  //   );
+  return (
+    <div className="Counter">
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+    </div>
+  );
 };
 
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-
 import Counter from './Counter';
-
 import './styles.scss';
 
 const Application = () => {
