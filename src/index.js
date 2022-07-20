543da4c1e1fed4da7b26507c4ddb806973867911
Shubham Mandal <shubm23@gmail.com>
2022-07-21 02:19:03 +0530

    Created Custom Hook

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -7,11 +7,38 @@ import CharacterList from './CharacterList';
 
 import dummyData from './dummy-data';
 
+import endpoint from './endpoint';
+
 import './styles.scss';
 
-const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
 
+const useFetch = (url) => {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setResponse(null);
+    setLoading(true);
+    setError(null);
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        setLoading(false);
+        setResponse(res);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err);
+      });
+  }, []);
+
+  return [response, loading, error];
+};
+
+const Application = () => {
+  const [response, loading, error] = useFetch(endpoint + '/characters');
+  const characters = response ? response.characters : [];
   return (
     <div className="Application">
       <header>
@@ -19,7 +46,12 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? (
+            <p>Loading....</p>
+          ) : (
+            <CharacterList characters={characters} />
+          )}
+          {error && <p className="error">{error.message}</p>}
         </section>
       </main>
     </div>
